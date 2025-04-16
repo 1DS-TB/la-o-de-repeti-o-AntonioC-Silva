5f603d2362e7bcb5b2d7e10e753499710e851179
Antonio Carlos Rodrigues da Silva <55711582801@>
2025-04-16 16:51:52 -0300

    Até o 7

---
+++ exercicio6.py
@@ -0,0 +1,13 @@
+num = int(input("Escreva um numero: "))
+lista = []
+somar = 0
+contador = 1
+if num<0:
+    print("invalido")
+else:
+    while contador<=num:
+        somar+=1
+        contador+=1
+        f = contador+contador
+        lista.append(f)
+print(lista)
---
+++ exercicio7.py
@@ -0,0 +1,8 @@
+num = int(input("Escreva um numero: "))
+vezes = ""
+while True:
+    for num in range(1, num+ 1):
+        vezes = vezes+"*"
+        print(vezes)
+    break
+
