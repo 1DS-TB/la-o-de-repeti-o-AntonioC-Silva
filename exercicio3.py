1254591b2886264716064d81cacaa1863332e4b9
AntonioC-Silva <acrscloud@gmail.com>
2025-05-04 20:38:01 -0300

    execercio3

--- exercicio3.py
+++ exercicio3.py
@@ -1,6 +1,3 @@
 num = int(input("Escreva o numero da tabuada que você quer saber: "))
-if num <0:
-    print("INVALIDO")
-else:
-    for tabuada in range(1,11):
-        print(f"{num} X {tabuada} = {num*tabuada}")
+for tabuada in range(1,11):
+    print(f"{num} X {tabuada} = {num*tabuada}")
